6588ac1f53ed7eccb97ac6d2dc075bde0e34a1ac
bcooperz1995 <brandon.cooper@mrisoftware.com>
2021-09-03 17:17:26 +1000

    reflect api

---
+++ reflect.js
@@ -0,0 +1,22 @@
+﻿
+
+
+const course = {
+    title: 'javascript - the complete guide'
+};
+
+Reflect.setPrototypeOf(course, { toString() { return this.title }});
+
+Reflect.defineProperty(course, 'price', {});
+
+// console.log(course.toString());
+
+const courseHandler = {
+    get(object, propertyName) {
+        console.log(propertyName);
+        return object[propertyName];
+    }
+};
+
+const pCourse = new Proxy(course, courseHandler);
+console.log(pCourse.title);
